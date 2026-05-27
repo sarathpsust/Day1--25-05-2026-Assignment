f1287378f8ef43671afc5306b30306309eb5b1c4
296822 <296822@ust.com>
2026-05-27 19:25:22 +0530

    student-crud created

---
+++ day2-assignment/student-crud/src/main/java/com/stuent/student_crud/StudentCrudApplication.java
@@ -0,0 +1,13 @@
+package com.stuent.student_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudApplication.class, args);
+	}
+
+}
